50a2bbca1eeec7ddaebbe38eb1d85b6acceb3774
IvayloGoranov <ivaylo.v.goranov@gmail.com>
2016-01-21 15:40:54 +0200

    Logger initial commit.

---
+++ OOP-Lab-Logger/Layouts/XMLLayout.cs
@@ -0,0 +1,21 @@
+﻿using Logger.Interfaces;
+using System;
+using System.Text;
+
+namespace Logger.Layouts
+{
+    public class XMLLayout : ILayout
+    {
+        public string Format(string msg, ReportLevel level, DateTime date)
+        {
+            var output = new StringBuilder();
+            output.AppendLine("<log>");
+            output.AppendLine("<date>" + date + "</date>");
+            output.AppendLine("<level>" + level + "</level>");
+            output.AppendLine("<message>" + msg + "</message>");
+            output.AppendLine("</log>");
+
+            return output.ToString();
+        }
+    }
+}
