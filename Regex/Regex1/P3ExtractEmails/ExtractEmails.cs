59cbd2ee88f9b77b4714b8ab5e0f8fc57159401d
Ivaylo Goranov <ivaylo.v.goranov@gmail.com>
2016-02-21 22:27:31 +0200

    Regex update.

--- Regex/Regex1/P3ExtractEmails/ExtractEmails.cs
+++ Regex/Regex1/P3ExtractEmails/ExtractEmails.cs
@@ -9,7 +9,8 @@ namespace P3ExtractEmails
         {
             string text = Console.ReadLine();
 
-            string pattern = @"[\w]+[\.-_]?[\w]@[\w]+[.-_]?[\w]\.[a-zA-Z]+\.?[a-zA-Z]*";
+            //string pattern = @"[\w]+[\.-_]?[\w]@[\w]+[.-_]?[\w]\.[a-zA-Z]+\.?[a-zA-Z]*";
+            string pattern = @"\s([a-zA-Z\d][a-zA-Z\d\.\-_]*[a-zA-Z\d]@(?:[a-zA-Z][a-zA-Z\-]*[a-zA-Z]\.){1,}[a-zA-Z][a-zA-Z\-]*[a-zA-Z])";
             Regex regex = new Regex(pattern);
             MatchCollection matches = regex.Matches(text);
 
