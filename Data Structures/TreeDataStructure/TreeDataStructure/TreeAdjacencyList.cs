3e5c2ee3afa7b08c94b576b5cecc64b935bd5334
Ivaylo Goranov <ivaylo.v.goranov@gmail.com>
2016-02-21 17:52:49 +0200

    Data Structures, Delegates, Extension methods initial commit

---
+++ Data Structures/TreeDataStructure/TreeDataStructure/TreeAdjacencyList.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TreeDataStructure
+{
+    public class AdjacencyListTree<T> : ITree<T>
+    {
+        private int nodesCount;
+        
+        private readonly List<T> visitedNodes = new List<T>();
+        
+        public AdjacencyListTree()
+        {
+            this.nodesCount = 0;
+            this.RootNode = null;
+        }
+
+        public AdjacencyListTree(TreeNode<T> rootNode)
+        {
+            this.RootNode = rootNode;
+        }
+        
+        public int NodesCount
+        {
+            get 
+            {
+                if (this.RootNode != null)
+                {
+                    this.CalculateNodesCountDFS(this.RootNode);    
+                }
+                
+                return this.nodesCount; 
+            }
+        }
+
+        public ITreeNode<T> RootNode { get; set; }
+
+        private void CalculateNodesCountDFS(ITreeNode<T> node)
+        {
+            if (visitedNodes.Contains(node.NodeName) == false)
+            {
+                visitedNodes.Add(node.NodeName);
+                foreach (var childNode in node.Children)
+                {
+                    CalculateNodesCountDFS(childNode);
+                }
+                this.nodesCount++;
+            }
+        }
+    }
+}
