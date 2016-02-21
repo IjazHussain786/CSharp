f358967ff08d134ed38d32e8b69f81987bac1f0d
Ivaylo Goranov <ivaylo.v.goranov@gmail.com>
2016-02-21 21:23:59 +0200

    Regex Initial Commit.

---
+++ Regex/Regex1/P3ExtractEmails/ExtractEmails.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace P3ExtractEmails
+{
+    class ExtractEmails
+    {
+        static void Main()
+        {
+            string text = Console.ReadLine();
+
+            string pattern = @"[\w]+[\.-_]?[\w]@[\w]+[.-_]?[\w]\.[a-zA-Z]+\.?[a-zA-Z]*";
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(text);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match);
+            }
+        }
+    }
+}
